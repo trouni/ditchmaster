d6e1a0d777dab523aea4df1fd6592bfb00230069
Trouni Tiet <trouni@gmail.com>
2020-11-25 17:59:20 +0900

    added verifications and converted to subprocess

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='gitronin',
-      version="1.0",
+      version="0.1",
       description="Replace master branch with main",
       packages=find_packages(),
       scripts=['gitronin'],
